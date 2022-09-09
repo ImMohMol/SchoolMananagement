4d571d24d26ff9e826139530c74def93fb5f7f6e
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-09-09 13:21:39 +0430

    initial commit

---
+++ src/main/java/com/schoolmanagement/schoolmanangemenet/SchoolManangemenetApplication.java
@@ -0,0 +1,13 @@
+package com.schoolmanagement.schoolmanangemenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManangemenetApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(SchoolManangemenetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/schoolmanagement/schoolmanangemenet/SchoolManangemenetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolmanagement.schoolmanangemenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManangemenetApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
