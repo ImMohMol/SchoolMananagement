52d91cfba361258bf81b8a45af9a7d1498c583d9
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-09-09 16:36:26 +0430

    crud service implementation created

---
+++ src/main/java/com/schoolmanagement/schoolmanangemenet/service/implementations/CrudService.java
@@ -0,0 +1,27 @@
+package com.schoolmanagement.schoolmanangemenet.service.implementations;
+
+import com.schoolmanagement.schoolmanangemenet.service.interfaces.ICrudService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CrudService implements ICrudService {
+    @Override
+    public void create () {
+
+    }
+
+    @Override
+    public void read () {
+
+    }
+
+    @Override
+    public void update () {
+
+    }
+
+    @Override
+    public void delete () {
+
+    }
+}
