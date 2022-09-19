8c1e6ac538492d7057f032613911277ce9b37083
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-09-20 00:27:32 +0430

    the general dto to model and model to dto mapper added

--- src/main/java/com/schoolmanagement/schoolmanangemenet/model/dto/mapper/GeneralMapper.java
+++ src/main/java/com/schoolmanagement/schoolmanangemenet/model/dto/mapper/GeneralMapper.java
@@ -1,7 +1,61 @@
-package com.schoolmanagement.schoolmanangemenet.model.dto.convertor;
+package com.schoolmanagement.schoolmanangemenet.model.dto.mapper;
 
-public class GeneralMapper<TSource, TDestination> {
-    public void convert (TSource source, TDestination destination) {
-        
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GeneralMapper {
+    public static <TSource, TDestination> void convert (TSource source, Type destinationType) {
+        try {
+            TDestination result = (TDestination) Class.forName(destinationType.getTypeName());
+            List<Method> sourceAccessorMethods = createAccessorMethods(source, true);
+            List<Method> destinationAccessorMethods = createAccessorMethods(result, false);
+            invokeMethods(sourceAccessorMethods, destinationAccessorMethods, source, result);
+        } catch (ClassNotFoundException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void invokeMethods (List<Method> sourceMethods, List<Method> destinationMethods, Object source,
+                                       Object destination) throws InvocationTargetException, IllegalAccessException {
+        for (Method handler : destinationMethods) {
+            for (Method sourceAccessorMethod : sourceMethods) {
+                if (sourceAccessorMethod.getName().substring(3).equals(handler.getName().substring(3))) {
+                    handler.invoke(destination, sourceAccessorMethod.invoke(source));
+                    break;
+                }
+            }
+        }
+    }
+
+    private static List<Method> createAccessorMethods (Object object, boolean isCreatingGetterMethods) {
+        Field[] fields = object.getClass().getDeclaredFields();
+        Method[] methods = object.getClass().getDeclaredMethods();
+        List<Method> result = new ArrayList<>();
+        for (Field field : fields)
+            addMethodToTheMethods(methods, result, createMethodName(field, isCreatingGetterMethods));
+        return result;
+    }
+
+    private static StringBuilder createMethodName (Field field, boolean isCreatingGetterMethods) {
+        StringBuilder fieldNameHandler = new StringBuilder(field.getName());
+        fieldNameHandler.replace(0, 0, String.valueOf(Character.toUpperCase(fieldNameHandler.charAt(0))));
+        fieldNameHandler.insert(0, isCreatingGetterMethods ? "get" : "set");
+        return fieldNameHandler;
+    }
+
+    private static void addMethodToTheMethods (Method[] methods, List<Method> result, StringBuilder methodNameHandler) {
+        for (Method method : methods) {
+            if (method.getName().equals(methodNameHandler.toString())) {
+                result.add(method);
+                break;
+            }
+        }
     }
 }
