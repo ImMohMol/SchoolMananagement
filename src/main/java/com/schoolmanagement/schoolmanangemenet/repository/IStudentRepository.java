33b298ef1fbaa3cbc096cec353047950665b86a5
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-09-20 14:37:14 +0430

    the findByNationalCode method added in the IStudentRepository

--- src/main/java/com/schoolmanagement/schoolmanangemenet/repository/IStudentRepository.java
+++ src/main/java/com/schoolmanagement/schoolmanangemenet/repository/IStudentRepository.java
@@ -3,5 +3,8 @@ package com.schoolmanagement.schoolmanangemenet.repository;
 import com.schoolmanagement.schoolmanangemenet.model.Student;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface IStudentRepository extends CrudRepository<Student, String> {
+    Optional<Student> findByNationalCode(String nationalCode);
 }
