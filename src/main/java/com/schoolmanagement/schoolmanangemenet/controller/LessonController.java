68a2c154e59939456e90bdfd19868397c0c3132e
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-09-21 11:59:21 +0430

    update lesosn added in the lesson controller

--- src/main/java/com/schoolmanagement/schoolmanangemenet/controller/LessonController.java
+++ src/main/java/com/schoolmanagement/schoolmanangemenet/controller/LessonController.java
@@ -4,6 +4,7 @@ import com.schoolmanagement.schoolmanangemenet.constant.GeneralConstantValues;
 import com.schoolmanagement.schoolmanangemenet.model.Response;
 import com.schoolmanagement.schoolmanangemenet.model.dto.lesson.CreateLessonDTO;
 import com.schoolmanagement.schoolmanangemenet.model.dto.lesson.ReadLessonsDTO;
+import com.schoolmanagement.schoolmanangemenet.model.dto.lesson.UpdateLessonDTO;
 import com.schoolmanagement.schoolmanangemenet.service.interfaces.ILessonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -32,5 +33,11 @@ public class LessonController {
     public ResponseEntity<Response> createNewLesson (@RequestBody @Valid CreateLessonDTO createLessonDTO) {
         this.lessonService.create(createLessonDTO);
         return ResponseEntity.ok(new Response(null, "Lesson created successfully!", null, true));
+    }
+
+    @PutMapping (path = "")
+    public ResponseEntity<Response> updateLesson (@RequestBody @Valid UpdateLessonDTO updateLessonDTO) {
+        this.lessonService.update(updateLessonDTO);
+        return ResponseEntity.ok(new Response(null, "Lesson updated successfully!", null, true));
     }
 }
