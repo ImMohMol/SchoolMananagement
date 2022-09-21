f1ecb93e6913bfb58a018ed9b073e4c7f07d9ec9
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-09-21 14:52:28 +0430

    update teacher method added in the teacher controller

--- src/main/java/com/schoolmanagement/controller/TeacherController.java
+++ src/main/java/com/schoolmanagement/controller/TeacherController.java
@@ -4,6 +4,7 @@ import com.schoolmanagement.constant.GeneralConstantValues;
 import com.schoolmanagement.model.Response;
 import com.schoolmanagement.model.dto.teacher.CreateTeacherDTO;
 import com.schoolmanagement.model.dto.teacher.ReadTeachersDTO;
+import com.schoolmanagement.model.dto.teacher.UpdateTeacherDTO;
 import com.schoolmanagement.service.interfaces.ITeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -33,5 +34,11 @@ public class TeacherController {
     public ResponseEntity<Response> createNewTeacher (@RequestBody @Valid CreateTeacherDTO createTeacherDTO) {
         this.teacherService.create(createTeacherDTO);
         return ResponseEntity.ok(new Response(null, "Teacher created successfully!", null, true));
+    }
+
+    @PutMapping (path = "")
+    public ResponseEntity<Response> updateTeacher (@RequestBody @Valid UpdateTeacherDTO updateTeacherDTO) {
+        this.teacherService.update(updateTeacherDTO);
+        return ResponseEntity.ok(new Response(null, "Teacher updated successfully!", null, true));
     }
 }
