0f28f31e58862b80fd6e4c00031ac798cad8ca7f
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-09-21 10:52:36 +0430

    create and update and read lesson dto classes created

--- src/main/java/com/schoolmanagement/schoolmanangemenet/model/dto/lesson/CreateLessonDTO.java
+++ src/main/java/com/schoolmanagement/schoolmanangemenet/model/dto/lesson/CreateLessonDTO.java
@@ -1,2 +1,28 @@
-package com.schoolmanagement.schoolmanangemenet.model.dto.lesson;public class CreatelessonDTO {
+package com.schoolmanagement.schoolmanangemenet.model.dto.lesson;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class CreateLessonDTO {
+    @NotNull (message = "The lesson name can't be null!")
+    private String name;
+    @NotNull (message = "The lesson grade can't be null!")
+    @Size (min = 1, max = 4, message = "The lesson grade should be greater than 0 and lower than 5")
+    private Integer gradeNumber;
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public Integer getGradeNumber () {
+        return gradeNumber;
+    }
+
+    public void setGradeNumber (Integer gradeNumber) {
+        this.gradeNumber = gradeNumber;
+    }
 }
--- src/main/java/com/schoolmanagement/schoolmanangemenet/model/dto/lesson/UpdateLessonDTO.java
+++ src/main/java/com/schoolmanagement/schoolmanangemenet/model/dto/lesson/UpdateLessonDTO.java
@@ -1,2 +1,38 @@
-package com.schoolmanagement.schoolmanangemenet.model.dto.lesson;public class UpdateLessonDTO {
+package com.schoolmanagement.schoolmanangemenet.model.dto.lesson;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class UpdateLessonDTO {
+    @NotNull (message = "The lesson id is needed to update the lesson information!")
+    private Integer id;
+    @NotNull (message = "The lesson name can't be null!")
+    private String name;
+    @NotNull (message = "The lesson grade can't be null!")
+    @Size (min = 1, max = 4, message = "The lesson grade should be greater than 0 and lower than 5")
+    private Integer gradeNumber;
+
+    public Integer getId () {
+        return id;
+    }
+
+    public void setId (Integer id) {
+        this.id = id;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public Integer getGradeNumber () {
+        return gradeNumber;
+    }
+
+    public void setGradeNumber (Integer gradeNumber) {
+        this.gradeNumber = gradeNumber;
+    }
 }
