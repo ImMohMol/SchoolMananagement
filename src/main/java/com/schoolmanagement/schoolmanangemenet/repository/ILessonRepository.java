ceb0f31040ea4d1f014a7d0047a1b4862f281785
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-09-21 10:53:07 +0430

    findByName method added in the lesson repository

--- src/main/java/com/schoolmanagement/schoolmanangemenet/repository/ILessonRepository.java
+++ src/main/java/com/schoolmanagement/schoolmanangemenet/repository/ILessonRepository.java
@@ -3,5 +3,8 @@ package com.schoolmanagement.schoolmanangemenet.repository;
 import com.schoolmanagement.schoolmanangemenet.model.Lesson;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ILessonRepository extends CrudRepository<Lesson, Integer> {
+    Optional<Lesson> findByName(String lessonName);
 }
