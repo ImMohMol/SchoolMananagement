1e5f4b7d35b7cde351af3427b00a259adc4de127
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-09-28 20:30:16 +0330

    methods for creating lesson messages added

--- src/main/java/com/schoolmanagement/constant/LessonMessageGenerator.java
+++ src/main/java/com/schoolmanagement/constant/LessonMessageGenerator.java
@@ -1,4 +1,23 @@
 package com.schoolmanagement.constant;
 
-public class LessonConstantValues {
+public class LessonMessageGenerator {
+    public static String createLessonExistsMessage (String lessonName) {
+        return String.format("Lesson (%s) exists and can't add it again!", lessonName);
+    }
+
+    public static String createLessonDoesNotExistMessage (String lessonName) {
+        return String.format("There is no lesson with name (%s)!", lessonName);
+    }
+
+    public static String createLessonCreatedMessage (String lessonName) {
+        return String.format("Lesson (%s) created successfully!", lessonName);
+    }
+
+    public static String createLessonUpdatedMessage (String lessonName) {
+        return String.format("Lesson (%s) updated successfully!", lessonName);
+    }
+
+    public static String createLessonDeletedMessage (String lessonName) {
+        return String.format("Lesson (%s) delete successfully!", lessonName);
+    }
 }
