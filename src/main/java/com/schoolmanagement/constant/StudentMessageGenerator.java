8d5ded5cd4d9ccde56ff757ce3dc3447fcbbeeb3
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-09-28 21:08:09 +0330

    StudentMessageGenerator methods added

--- src/main/java/com/schoolmanagement/constant/StudentMessageGenerator.java
+++ src/main/java/com/schoolmanagement/constant/StudentMessageGenerator.java
@@ -1,7 +1,31 @@
 package com.schoolmanagement.constant;
 
-public class StudentConstantValues {
-    public final static String STUDENT_DELETED_SUCCESSFULLY = "Student deleted successfully!";
-    public final static String STUDENT_CREATED_SUCCESSFULLY = "student created successfully!";
-    public final static String STUDENT_UPDATED_SUCCESSFULLY = "student updated successfully!";
+public class StudentMessageGenerator {
+    public static String createStudentExistsMessage (String firstName, String lastName) {
+        return String.format("Student (%s %s) exists and can't add it again!", firstName, lastName);
+    }
+
+    public static String createStudentDoesNotExistMessage (String firstName, String lastName) {
+        return String.format("Student (%s %s) does not exist!", firstName, lastName);
+    }
+
+    public static String createStudentDoesNotExistMessage (String studentNo) {
+        return String.format("Student with studentNo (%s) does not exist!", studentNo);
+    }
+
+    public static String createStudentDoesNotHaveEnrolledLessonsMessage (String studentNo) {
+        return String.format("Student with studentNo (%s) does not have any enrolled lessons!", studentNo);
+    }
+
+    public static String createStudentCreatedMessage (String firstName, String lastName) {
+        return String.format("Student (%s %s) created successfully!", firstName, lastName);
+    }
+
+    public static String createStudentUpdateddMessage (String firstName, String lastName) {
+        return String.format("Student (%s %s) updated successfully!", firstName, lastName);
+    }
+
+    public static String createStudentDeletedMessage (String studentNo) {
+        return String.format("Student (%s) created successfully!", studentNo);
+    }
 }
