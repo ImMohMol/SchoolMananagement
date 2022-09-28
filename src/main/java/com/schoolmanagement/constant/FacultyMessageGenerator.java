1686d1efaa07d2d9b613b40505f04d64ba472097
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-09-28 20:13:43 +0330

    some methods to create messages added

--- src/main/java/com/schoolmanagement/constant/FacultyMessageGenerator.java
+++ src/main/java/com/schoolmanagement/constant/FacultyMessageGenerator.java
@@ -5,11 +5,19 @@ public class FacultyMessageGenerator {
         return String.format("Faculty (%s) exists and can't add it again!", facultyName);
     }
 
-    public static String createFacultyDoesNotExists (String facultyName) {
+    public static String createFacultyDoesNotExistsMessage (String facultyName) {
         return String.format("There is no faculty with name (%s)!", facultyName);
     }
 
-    public static String createFacultyDoesNotExists (Long facultyId) {
-        return String.format("There is no faculty with id (%s)!", facultyId);
+    public static String createFacultyCreatedMessage (String facultyName) {
+        return String.format("Faculty (%s) created successfully!", facultyName);
+    }
+
+    public static String createFacultyUpdatedMessage (String facultyName) {
+        return String.format("Faculty (%s) information updated successfully!", facultyName);
+    }
+
+    public static String createFacultyDeletedMessage (String facultyName) {
+        return String.format("Faculty (%s) delete successfully!", facultyName);
     }
 }
